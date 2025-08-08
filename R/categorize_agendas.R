b8a29b2fbfb59e2037e2a13118a3f978edd96883
Michiel Duvekot <michiel@duvekot.ca>
2025-08-07 21:18:40 -0400

    fix categorize agendas to handle NULL results from fetch_individual_meeting_agenda_item_titles

--- R/categorize_agendas.R
+++ R/categorize_agendas.R
@@ -8,17 +8,18 @@
 #' if(interactive()){
 #'  results <- categorize_agendas(2467)
 #'  results <- categorize_agendas(2566)
+#'  results <- categorize_agendas(2945)
+#'  results <- categorize_agendas(2946)
+#' 
+#' decisionBodyId = 2945
+#' fetch_individual_meeting_agenda_item_titles(2945) 
+#' meetingId = 26585
+#' meetingId = 27110
 #'  View(results$categorized)
 #'  View(results$unmatched)
 #'
 #'  }
 #' }
-#' @seealso
-#'  \code{\link[purrr]{map}}, \code{\link[purrr]{list_c}}
-#'  \code{\link[dplyr]{mutate}}, \code{\link[dplyr]{filter}}
-#'  \code{\link[stringr]{str_detect}}, \code{\link[stringr]{modifiers}}
-#'  \code{\link[tibble]{tibble}}
-#'  \code{\link[tidyr]{unnest}}
 #' @rdname categorize_agendas
 #' @export
 #' @importFrom purrr map list_rbind map_lgl
@@ -29,20 +30,42 @@
 #'
 categorize_agendas <- function(decisionBodyId) {
   # Fetch meetings
-  # decisionBodyId = 2566
+  # decisionBodyId = 2945
+  # decisionBodyId <- 29
   meetings <- fetch_multiple_meeting(decisionBodyId = decisionBodyId)
   meetingIds <- meetings$meetingId # |> tail(3)
 
+  # decisionBodyId <- 2944
+  #   fetch_individual_meeting_agenda_item_titles()
   # Fetch agenda item titles
-  agenda_raw <- purrr::map(
-    meetingIds,
-    fetch_individual_meeting_agenda_item_titles
-  ) |>
-    purrr::list_rbind()
+  # agenda_raw <<- purrr::map(
+  #   meetingIds,
+  #   fetch_individual_meeting_agenda_item_titles
+  # ) |>
+  #   purrr::list_rbind()
+agenda_raw <- purrr::map(
+  meetingIds,
+  fetch_individual_meeting_agenda_item_titles
+)
+
+# Filter out NULLs
+agenda_raw <- purrr::compact(agenda_raw)
+
+# If all results were NULL, return an empty tibble with expected columns
+if (length(agenda_raw) == 0) {
+  agenda_raw <- tibble::tibble(
+    nativeTermYear = integer(),
+    referenceNumber = character(),  # Add other expected columns here
+    agendaItemTitle = character()
+  )
+} else {
+  agenda_raw <<- purrr::list_rbind(agenda_raw)
+}
 
   # categorize by sector
   agenda_categorized <- agenda_raw |>
     dplyr::mutate(
+      # store the results in a tibble, then unnest to make key_term and sector columns
       sector_info = purrr::map(
         .data[["agendaItemTitle"]],
         function(text) {
@@ -55,7 +78,11 @@ categorize_agendas <- function(decisionBodyId) {
               ignore_case = FALSE
             )
           )
-          if (!any(detected)) return(NULL)
+          #if (!any(detected)) return(NULL)
+          if (!any(detected)) return(tibble::tibble(
+            key_term = NA,
+            sector = NA
+          ))
           tibble::tibble(
             key_term = terms_tbl$key_terms[detected],
             sector = terms_tbl$sectors[detected]
@@ -65,29 +92,33 @@ categorize_agendas <- function(decisionBodyId) {
     ) |>
     tidyr::unnest(.data$sector_info)
 
+  
   # unmatched items
-  agenda_unmatched <- agenda_raw |>
-    dplyr::mutate(
-      sector_info = purrr::map(
-        .data$agendaItemTitle,
-        function(text) {
-          terms_tbl <- key_terms()
-          detected <- stringr::str_detect(
-            text,
-            stringr::regex(terms_tbl$key_terms, ignore_case = FALSE)
-          )
-          if (!any(detected)) return(NULL)
-          tibble::tibble(
-            key_term = terms_tbl$key_terms[detected],
-            sector = terms_tbl$sectors[detected]
-          )
-        }
-      )
-    ) |>
-    dplyr::filter(purrr::map_lgl(.data$sector_info, is.null))
+  # agenda_unmatched <- agenda_raw |>
+  #   dplyr::mutate(
+  #     sector_info = purrr::map(
+  #       .data$agendaItemTitle,
+  #       function(text) {
+  #         terms_tbl <- key_terms()
+  #         detected <- stringr::str_detect(
+  #           text,
+  #           stringr::regex(terms_tbl$key_terms, ignore_case = FALSE)
+  #         )
+  #         if (!any(detected)) return(NULL)
+  #         tibble::tibble(
+  #           key_term = terms_tbl$key_terms[detected],
+  #           sector = terms_tbl$sectors[detected]
+  #         )
+  #       }
+  #     )
+  #   ) |>
+  #   dplyr::filter(purrr::map_lgl(.data$sector_info, is.null)) |> 
+  #   dplyr::rename(sector = sector_info) |> 
+  #   dplyr::mutate(key_term = NA) |>
+  #   dplyr::select(c("nativeTermYear","referenceNumber","agendaItemTitle","key_term","sector"))
 
   list(
-    categorized = agenda_categorized,
-    unmatched = agenda_unmatched
+    categorized = agenda_categorized
+    #unmatched = agenda_unmatched
   )
 }
