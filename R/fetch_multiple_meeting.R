4a087c640c436851da8b306f163f406440327be5
Michiel Duvekot <michiel@duvekot.ca>
2025-08-15 13:25:29 -0400

    add closure-based chacing to fetch_mulitple_meeting

--- R/fetch_multiple_meeting.R
+++ R/fetch_multiple_meeting.R
@@ -6,7 +6,11 @@
 #' @examples
 #' \dontrun{
 #' if(interactive()){
-#'  meetings_2566 <- fetch_multiple_meeting(decisionBodyId = 2566)
+#'  meetings_2562 <- make_fetch_meetings()(decisionBodyId = 2562) # creates a new instance with its own cache
+#'  fetcher <- make_fetch_meetings() # store the closure
+#'  fetcher(2562)
+#'  fetcher(2566)
+#'
 #'  }
 #' }
 #' @seealso
@@ -15,13 +19,37 @@
 #' @export
 #' @importFrom jsonlite fromJSON
 #'
-fetch_multiple_meeting <- function(decisionBodyId = 2566) {
-  # https://secure.toronto.ca/council/api/multiple/meeting.json?decisionBodyId=2566
-  meeting_list <- jsonlite::fromJSON(
-    paste0(
+# without caching
+# fetch_multiple_meeting <- function(decisionBodyId = 2566) {
+#   # https://secure.toronto.ca/council/api/multiple/meeting.json?decisionBodyId=2566
+#   meeting_list <- jsonlite::fromJSON(
+#     paste0(
+#       "https://secure.toronto.ca/council/api/multiple/meeting.json?decisionBodyId=",
+#       decisionBodyId
+#     )
+#   )
+#   meeting_list$Records
+# }
+
+# with closure-based caching
+make_fetch_meetings <- function() {
+  cache <- list()
+
+  function(decisionBodyId) {
+    key <- as.character(decisionBodyId)
+
+    if (!is.null(cache[[key]])) {
+      message("Using cached data for decisionBodyId: ", decisionBodyId)
+      return(cache[[key]])
+    }
+
+    url <- paste0(
       "https://secure.toronto.ca/council/api/multiple/meeting.json?decisionBodyId=",
       decisionBodyId
     )
-  )
-  meeting_list$Records
+
+    meeting_list <- jsonlite::fromJSON(url)
+    cache[[key]] <<- meeting_list$Records
+    meeting_list$Records
+  }
 }
